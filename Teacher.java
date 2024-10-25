a2c9d95d3c8dcbec383a8ba04bc28bbfea66ed83
coco4321 <96091204+coco4321@users.noreply.github.com>
2024-10-25 15:43:16 +0800

    Create Teacher.java

---
+++ Teacher.java
@@ -0,0 +1,9 @@
+public Class Teacher{
+    public static void main(String[] args) {
+        int sum = getResult(5, 7);
+        System.out.println("The sum : " + sum);
+    }
+
+    //请写出一个方法，实现两个整数相加的功能
+
+}
